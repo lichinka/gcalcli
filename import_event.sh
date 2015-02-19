fd0cdad8f18d7196f95f870934881ce6908ccf7e
Lucas Benedicic <lucas.benedicic@gmail.com>
2015-02-19 17:11:40 +0100

    Improved scripts after scriptcheck.net.-

--- import_event.sh
+++ import_event.sh
@@ -4,10 +4,10 @@ EVENT=$1
 CAL=Laburo
 
 if [ -n "${EVENT}" ]; then
-    SUMMARY="$( grep '^SUMMARY' ${EVENT} | cut -d':' -f2 )"
+    SUMMARY="$( grep '^SUMMARY' "${EVENT}" | cut -d':' -f2 )"
     FOUND="$( ./gcalcli --calendar ${CAL} search "${SUMMARY}" | grep -i 'no events found' )"
     if [ -n "${FOUND}" ]; then
-        ./gcalcli --calendar ${CAL} import ${EVENT}
+        ./gcalcli --calendar ${CAL} import "${EVENT}"
     else
         echo "[WARNING] Not importing ${EVENT} because its summary was found in <${CAL}>"
     fi
--- parse_ical.sh
+++ parse_ical.sh
@@ -7,24 +7,25 @@ if [ -n "${LOG}" ]; then
     #
     # create a from-to file with the lines numbers of each event
     #
-    egrep -n "(BEGIN|END)\:VCALENDAR" ${LOG} | grep 'BEGIN:' | awk 'BEGIN { FS=":"; } { print $1 }' > /tmp/.from
-    egrep -n "(BEGIN|END)\:VCALENDAR" ${LOG} | grep 'END:' | awk 'BEGIN { FS=":"; } { print $1 }' > /tmp/.to
+    egrep -n "(BEGIN|END)\:VCALENDAR" "${LOG}" | grep 'BEGIN:' | awk 'BEGIN { FS=":"; } { print $1 }' > /tmp/.from
+    egrep -n "(BEGIN|END)\:VCALENDAR" "${LOG}" | grep 'END:' | awk 'BEGIN { FS=":"; } { print $1 }' > /tmp/.to
     paste -d"${DEL}" /tmp/.from /tmp/.to > /tmp/.events
 
     #
     # start extracting events
     #
-    for I in $( cat /tmp/.events ); do
-        BEG="$( echo "${I}" | cut -d"${DEL}" -f1 )"
-        END="$( echo "${I}" | cut -d"${DEL}" -f2 )"
+    while read line
+    do
+        BEG="$( echo "${line}" | cut -d"${DEL}" -f1 )"
+        END="$( echo "${line}" | cut -d"${DEL}" -f2 )"
         if [ -n "${BEG}" ] && [ -n "${END}" ]; then
             OUT="/tmp/.${END}.ical"
-            cat ${LOG} | awk "NR >= ${BEG} && NR <= ${END}" > ${OUT}
+            awk "NR >= ${BEG} && NR <= ${END}" "${LOG}" > "${OUT}"
             echo "[INFO] Event ${OUT} created"
         else
-            echo "[WARNING] Invalid interval ${I}"
+            echo "[WARNING] Invalid interval ${line}"
         fi
-    done
+    done < /tmp/.events
 else
     echo "Usage: $0 [DavMail log]"
     echo "Parses individual iCal events from a DavMail log file.-"
